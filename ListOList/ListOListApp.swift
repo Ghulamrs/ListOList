75d4316b8e3bb203ffe7c7296f4cbec1245cccf0
Ghulam Rasool <g_r_akhtar@yahoo.com>
2021-10-15 20:33:49 +0500

    Initial Commit

---
+++ ListOList/ListOListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOListApp.swift
+//  ListOList
+//
+//  Created by Home on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ListOListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
